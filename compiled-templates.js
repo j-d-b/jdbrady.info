0523f037c926233020293765777941493beb87d6
j-d-b <jacob@jdbrady.info>
2018-03-03 00:49:35 -0500

    removed main.css and added content to base.scss

---
+++ compiled-templates.js
@@ -0,0 +1,18 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['projects'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+
+  return "  <div class=\"card border-gray rounded p-3 mb-2 mt-2\" data-target=\""
+    + alias4(((helper = (helper = helpers.href || (depth0 != null ? depth0.href : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"href","hash":{},"data":data}) : helper)))
+    + "\">\n    <span class=\"font-reg font-weight-bold\">"
+    + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
+    + "</span>\n    <p class=\"mb-0\">"
+    + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
+    + "</p>\n  </div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.projects : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
+})();
---
+++ main.js
@@ -0,0 +1,72 @@
+// Jacob Brady
+
+
+var pageHashes = ['#home', '#products', '#contact'];
+var currentHash = '#home';
+var defaultHash = '#home'; // could link to page not found, etc.
+
+// $(document).ready(function(){
+//   currentHash = checkHash( $(location).attr('hash') );
+//   displayContent(currentHash);
+//   setHighlight(currentHash);
+//
+//   $(window).on('hashchange', function() {
+//     var newHash = checkHash( $(location).attr('hash') );
+//     switchHighlight(newHash);
+//     displayContent(newHash);
+//     currentHash = newHash;
+//   });
+// });
+
+
+$(document).ready(function() {
+  $('#home-content').load('projects.html', function() {
+    loadProj();
+  });
+
+  $('.nav-a').on('click', function() {
+    var page = $(this).attr('data-target');
+    console.log(page);
+    var path = page + '.html';
+
+    $('#home-content').load(path, function() {
+      if(page == 'projects') {
+        loadProj();
+      }
+    });
+
+    if ($(this).hasClass('nav-active')){
+      return;
+    }
+    else {
+      $('.nav-a').removeClass('nav-active');
+      $(this).addClass('nav-active');
+    }
+  });
+
+});
+
+// changes the menu highlight to match the given view
+function switchHighlight(hash) {
+  if (hash != currentHash) {
+    setHighlight(hash);
+  }
+}
+
+// highlights only navbar item corresponding to the given hash
+function setHighlight(hash) {
+  $('.nav-item').removeClass('nav-active');
+  $('a[href="' + hash + '"]').addClass('nav-active');
+}
+
+function loadProj() {
+  $.getJSON('projects.json', function(context) {
+    var templateScript = Handlebars.templates.projects(context);
+    $('#projects').html(templateScript);
+
+    $('.card').on('click', function(){
+      var href = $(this).attr('data-target');
+      window.location = href;
+    });
+  });
+}
