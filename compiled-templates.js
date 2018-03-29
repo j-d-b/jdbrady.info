ba2cd3b67a93f5be36d4b5df7792af379d6f005b
j-d-b <jacob@jdbrady.info>
2018-03-29 01:00:40 -0400

    kt photo, temporary link to obsv_imgs.html

--- compiled-templates.js
+++ compiled-templates.js
@@ -1,15 +1,26 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+templates['obsv'] = template({"1":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return "  <div class=\"col-md-3 col-lg-2 p-1 obsv-img\">\n    <img class=\"img-fluid rounded\" src=\""
+    + container.escapeExpression(((helper = (helper = helpers.source || (depth0 != null ? depth0.source : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"source","hash":{},"data":data}) : helper)))
+    + "\"></img>\n  </div>\n";
+},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0 != null ? depth0 : (container.nullContext || {}),(depth0 != null ? depth0.images : depth0),{"name":"each","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "");
+},"useData":true});
 templates['projects'] = template({"1":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "  <div class=\"card border-gray rounded p-3 mb-2 mt-2\" data-target=\""
+  return "  <a href=\""
     + alias4(((helper = (helper = helpers.href || (depth0 != null ? depth0.href : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"href","hash":{},"data":data}) : helper)))
-    + "\">\n    <span class=\"font-reg font-weight-bold\">"
+    + "\">\n    <div class=\"project card border-gray rounded p-3 mb-2 mt-2\">\n      <span class=\"font-reg font-weight-bold\">"
     + alias4(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</span>\n    <p class=\"mb-0\">"
+    + "</span>\n      <p class=\"mb-0 proj-desc\">"
     + ((stack1 = ((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"description","hash":{},"data":data}) : helper))) != null ? stack1 : "")
-    + "</p>\n  </div>\n";
+    + "</p>\n    </div>\n  </a>\n";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1;
 
--- main.js
+++ main.js
@@ -1,54 +1,57 @@
 // Jacob Brady
 
+const projHashes = ['#time', '#worldtrends', '#sketches', '#spacecat', '#survivors', '#observatory', '#surf', '#kombucha' ];
+const navHashes = ['#projects', '#about', '#photo'];
+const validHashes = projHashes.concat(navHashes);
+const defaultHash = '#projects';
 
-var pageHashes = ['#home', '#products', '#contact'];
-var currentHash = '#home';
-var defaultHash = '#home'; // could link to page not found, etc.
-var currPage = 'projects';
+var currentHash = '#projects';
 
-// $(document).ready(function(){
-//   currentHash = checkHash( $(location).attr('hash') );
-//   displayContent(currentHash);
-//   setHighlight(currentHash);
-//
-//   $(window).on('hashchange', function() {
-//     var newHash = checkHash( $(location).attr('hash') );
-//     switchHighlight(newHash);
-//     displayContent(newHash);
-//     currentHash = newHash;
-//   });
-// });
+$(document).ready(function(){
+  currentHash = checkValid( $(location).attr('hash') );
+  displayContent(currentHash);
+  setHighlight(currentHash);
 
-
-$(document).ready(function() {
-  $('#home-content').load('projects.html', function() {
-    loadProj();
+  $(window).on('hashchange', function() {
+    var newHash = checkValid( $(location).attr('hash') );
+    switchHighlight(newHash);
+    displayContent(newHash);
+    currentHash = newHash;
   });
-
-  $('.nav-a').on('click', function() {
-    var page = $(this).attr('data-target');
-    if(page != currPage){
-      currPage = page;
-      var path = page + '.html';
-      $('#home-content').load(path, function() {
-        if(page == 'projects') {
-          loadProj();
-        }
-      });
-
-      if ($(this).hasClass('nav-active')){
-        return;
-      }
-      else {
-        $('.nav-a').removeClass('nav-active');
-        $(this).addClass('nav-active');
-      }
-    }
-  });
-
 });
 
-// changes the menu highlight to match the given view
+function hashToPath(hash) {
+  return '/_' + hash.substring(1) + '.html'; // prefix partials w/ underscore
+}
+
+function checkValid(hash) {
+  var isValid = $.inArray(hash, validHashes) != -1;
+  if (isValid) {
+    return hash;
+  }
+  else {
+    window.location.hash = defaultHash;
+    return defaultHash;
+  }
+}
+
+function displayContent(hash) {
+  if(hash == '#astro'){
+    return;
+  }
+
+  var path = hashToPath(hash);
+  $('#home-content').load(path, function() {
+    if (hash == '#projects') {
+      loadProj();
+    }
+    else if (hash == '#observatory') {
+      loadObsvImgs();
+    }
+  });
+}
+
+// changes the menu highlight to match the given hash
 function switchHighlight(hash) {
   if (hash != currentHash) {
     setHighlight(hash);
@@ -57,18 +60,41 @@ function switchHighlight(hash) {
 
 // highlights only navbar item corresponding to the given hash
 function setHighlight(hash) {
-  $('.nav-item').removeClass('nav-active');
+  if( $.inArray(hash, projHashes) != -1) {
+    hash = '#projects';
+  }
+  $('.nav-a').removeClass('nav-active');
   $('a[href="' + hash + '"]').addClass('nav-active');
 }
+
 
 function loadProj() {
   $.getJSON('projects.json', function(context) {
     var templateScript = Handlebars.templates.projects(context);
     $('#projects').html(templateScript);
 
-    $('.card').on('click', function(){
-      var href = $(this).attr('data-target');
-      window.location = href;
+    $('a[href="files/astro_proc_guide_nov2016.pdf"]').hover(
+      function() {
+        var currHeight = $('.project', 'a[href="files/astro_proc_guide_nov2016.pdf"]').height();
+        $('.project', 'a[href="files/astro_proc_guide_nov2016.pdf"]').height(currHeight);
+        $('.proj-desc', this).html('<div class="text-center pt-3"><p class="font-lg pb-0 mb-0"><i class="fas fa-file-pdf"></i></p><p>Click to view PDF<p></div>');
+        window.location.hash = '#projects';
+      },
+      function() {
+        $('.project', 'a[href="files/astro_proc_guide_nov2016.pdf"]').css('height', 'auto');
+        $('.proj-desc', this).html('Reference manual for astronomical image processing workflow using SAOImage DS9 and Adobe Lightroom');
+      }
+    );
+
+  });
+}
+
+function loadObsvImgs() {
+  $.getJSON('obsv-imgs.json', function(context) {
+    var templateScr = Handlebars.templates.obsv(context);
+    $('#obsv-imgs').html(templateScr);
+    $('.obsv-img').on('click', function() {
+      window.location = 'obsv_imgs.html';
     });
   });
 }
